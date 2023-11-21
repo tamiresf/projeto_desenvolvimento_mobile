ba16da7f2cdb2140c8103dc94b854b1fbbbd2c01
Tamires Ferreira <taamiresferreiraa@hotmail.com>
2023-11-20 23:38:03 -0300

    Criação da screen Perfil

--- projetoDesenvolvimentoMobile/src/routes/index.jsx
+++ projetoDesenvolvimentoMobile/src/routes/index.jsx
@@ -2,10 +2,11 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import Detalhes from '../screens/Detalhes';
+import Detalhes from '../screens/detalhes';
 import Home from '../screens/Home';
 import EsqueciSenha from '../screens/EsqueciSenha';
 import Produtos from '../screens/Produtos';
+import Perfil from '../screens/Perfil';
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
@@ -13,10 +14,11 @@ const Rotas = () => {
     return (
         <NavigationContainer>
             <Navigator screenOptions={{ headerShown: false }}>
-                <Screen name="Produtos" component={Produtos} />
-                <Screen name="Detalhes" component={Detalhes} />
-                <Screen name="EsqueciSenha" component={EsqueciSenha} />
+                <Screen name="Perfil" component={Perfil} />
                 <Screen name="Home" component={Home} />
+                <Screen name="EsqueciSenha" component={EsqueciSenha} />
+                <Screen name="Detalhes" component={Detalhes} />
+                <Screen name="Produtos" component={Produtos} />
             </Navigator>
         </NavigationContainer >
     );
