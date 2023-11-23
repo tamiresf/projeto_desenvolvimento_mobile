3dfcbc4aa27c4239e227c9d05e278cc0abfd67cf
paulotulerfaeterj <paulogustavotuler@gmail.com>
2023-11-22 22:13:03 -0300

    Estilização do front-end

--- projetoDesenvolvimentoMobile/src/screens/ProdutosFE/index.jsx
+++ projetoDesenvolvimentoMobile/src/screens/ProdutosFE/index.jsx
@@ -1,8 +1,17 @@
-import { View, Text, FlatList, Image, StyleSheet, Button, TextInput, TouchableOpacity, } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  Image,
+  StyleSheet,
+  Button,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 import React, { useEffect, useState } from "react";
 import { api } from "../../service/api";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-
+import { AntDesign } from '@expo/vector-icons';
 
 const ProdutosFE = ({ navigation }) => {
   const [listaProdutos, setListaProdutos] = useState([]);
@@ -21,25 +30,22 @@ const ProdutosFE = ({ navigation }) => {
     getProdutos();
   }, []);
 
-
   const gardarInfo = async (id) => {
     try {
       AsyncStorage.clear();
-      await AsyncStorage.setItem('idDoProduto', JSON.stringify(id));
+      await AsyncStorage.setItem("idDoProduto", JSON.stringify(id));
       console.log(id);
-      navigation.navigate('DetalhesFE');
+      navigation.navigate("DetalhesFE");
     } catch (e) {
       console.log(e);
     }
-  }
+  };
 
   return (
     <View style={styles.produtos}>
       <View style={styles.header}>
-        <TextInput placeholder="Buscar produto" style={styles.campo}
-
-        />
-
+      <AntDesign name="search1" size={24} color="black" style={{alignSelf: 'center'}} />
+        <TextInput placeholder="Buscar produto" style={{...styles.campo,marginLeft: 10, }} />
       </View>
       {listaProdutos.length > 0 ? (
         <FlatList
@@ -48,12 +54,16 @@ const ProdutosFE = ({ navigation }) => {
           numColumns={2}
           renderItem={({ item }) => (
             <View style={styles.lista}>
-              <Text style={{ fontSize: 20 }}> {item.nome} </Text>
+              <Text style={{ fontSize: 30, fontWeight: '700' }}> {item.nome} </Text>
 
-              <Image height={100} width={100} resizeMode="contain" source={{ uri: item?.imagem }} />
-              <TouchableOpacity
-
-                onPress={() => gardarInfo(item.id)}>
+              <Image
+                height={300}
+                width={300}
+                resizeMode="contain"
+                source={{ uri: item?.imagem }}
+              />
+              <Text style={{fontSize: 50, marginBottom: 20}}>R${item.preco}</Text>
+              <TouchableOpacity onPress={() => gardarInfo(item.id)}>
                 <Text style={styles.ver}> Ver Mais</Text>
               </TouchableOpacity>
             </View>
@@ -63,8 +73,7 @@ const ProdutosFE = ({ navigation }) => {
         <Text>Nenhum produto encontrado</Text>
       )}
 
-      <View style={styles.footer}>
-      </View>
+      {/* <View style={styles.footer}></View> */}
     </View>
   );
 };
@@ -74,37 +83,42 @@ export default ProdutosFE;
 const styles = StyleSheet.create({
   produtos: {
     flex: 1,
+
   },
   lista: {
-    flex: 1,
-    backgroundColor: "#aaa",
+    flex: 0,
+    backgroundColor: "snow",
     margin: 5,
     justifyContent: "center",
     alignItems: "center",
-    padding: 5,
+    padding: 50,
+    
     borderRadius: 7,
+    width: '100%'
   },
   header: {
     marginTop: 40,
-    height: "10%",
+    height: "5%",
     width: "100%",
-    justifyContent: "center",
+    // justifyContent: "center",
+    flexDirection: 'row',
     // alignItems: "center",
     // backgroundColor: "blue",
+    borderWidth: 2,
+    borderRadius: 100,
   },
   footer: {
     height: "7%",
     width: "100%",
     backgroundColor: "blue",
-
   },
 
   ver: {
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     borderRadius: 8,
     padding: 16,
     marginBottom: 16,
-    shadowColor: '#000',
+    shadowColor: "#000",
     shadowOffset: {
       width: 0,
       height: 2,
@@ -112,22 +126,24 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
     elevation: 5,
+    borderColor: 'black',
+    borderWidth: 1
   },
   productName: {
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 8,
   },
   button: {
-    backgroundColor: '#0A8DD8',
+    backgroundColor: "#0A8DD8",
     paddingVertical: 6,
     paddingHorizontal: 12,
     borderRadius: 4,
-    alignSelf: 'flex-start',
+    alignSelf: "flex-start",
   },
   buttonText: {
-    color: '#fff',
-    fontWeight: 'bold',
+    color: "#fff",
+    fontWeight: "bold",
     paddingTop: 6,
   },
 });
