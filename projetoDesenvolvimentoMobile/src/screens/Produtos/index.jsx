102f7fe2249d546bd2c7f5920cec9ee60874430d
WallaceHenriques <walaci@hotmail.com>
2023-11-23 16:21:36 -0300

    implementando o botão adicionar produto

--- projetoDesenvolvimentoMobile/src/screens/Produtos/index.jsx
+++ projetoDesenvolvimentoMobile/src/screens/Produtos/index.jsx
@@ -1,9 +1,17 @@
-import { View, Text, FlatList, Image, StyleSheet, Button, TextInput, TouchableOpacity, } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  Image,
+  StyleSheet,
+  Button,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 import React, { useCallback, useEffect, useState } from "react";
 import { api } from "../../service/api";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useFocusEffect } from "@react-navigation/core";
-
 
 const Produtos = ({ navigation }) => {
   const [listaProdutos, setListaProdutos] = useState([]);
@@ -18,32 +26,47 @@ const Produtos = ({ navigation }) => {
     }
   };
 
-  useFocusEffect(useCallback(() => {
-    getProdutos()
-  }, []))
-
+  useFocusEffect(
+    useCallback(() => {
+      getProdutos();
+    }, [])
+  );
 
   const gardarInfo = async (id) => {
     try {
       AsyncStorage.clear();
-      await AsyncStorage.setItem('idDoProduto', JSON.stringify(id));
+      await AsyncStorage.setItem("idDoProduto", JSON.stringify(id));
       console.log(id);
-      navigation.navigate('Detalhes');
+      navigation.navigate("Detalhes");
     } catch (e) {
       console.log(e);
     }
-  }
+  };
+  const novoProduto = async () => {
+    try {
+      const novoProduto ={
+       nome: "nome do produto",
+       imagem: "url da imagem do produto",
+       descricao: "descrição do produto",
+       categoria: "categoria do produto",
+       preco: "preco do produto",
 
+      }
+      const {data} = await api.post("/produtos",novoProduto);
+      console.log(data);
+      AsyncStorage.clear();
+      await AsyncStorage.setItem("idDoProduto", JSON.stringify(data.id));
+      navigation.navigate ("Detalhes");
+    } catch (e) {
+      console.log(e);
+    }
+  };
   return (
     <View style={styles.produtos}>
       <View style={styles.header}>
-        <TextInput placeholder="Buscar produto" style={styles.campo}
-
-        />
-
+        <TextInput placeholder="Buscar produto" style={styles.campo} />
       </View>
       {listaProdutos.length > 0 ? (
-        
         <FlatList
           data={listaProdutos}
           keyExtractor={(item) => item.id.toString()}
@@ -52,30 +75,30 @@ const Produtos = ({ navigation }) => {
             <View style={styles.lista}>
               <Text style={{ fontSize: 20 }}> {item.nome} </Text>
 
-              <Image height={100} width={100} resizeMode="contain" source={{ uri: item?.imagem }} />
-              <TouchableOpacity
-
-                onPress={() => gardarInfo(item.id)}>
+              <Image
+                height={100}
+                width={100}
+                resizeMode="contain"
+                source={{ uri: item?.imagem }}
+              />
+              <TouchableOpacity onPress={() => gardarInfo(item.id)}>
                 <Text style={styles.ver}> Ver Mais</Text>
               </TouchableOpacity>
             </View>
           )}
-            style={{width: '100%',}}
+          style={{ width: "100%" }}
         />
-        
-        
       ) : (
         <Text>Nenhum produto encontrado</Text>
       )}
 
       <View style={styles.footer}>
-
-       <TouchableOpacity style={styles.adicionar}
-      //  onPress={navigation.navigate ("Detalhes")}
-       >
-       <Text style={styles.adicionarText}>Adicionar Produto</Text>
-       </TouchableOpacity>
-
+        <TouchableOpacity
+          style={styles.adicionar}
+          onPress={() => novoProduto()}
+        >
+          <Text style={styles.adicionarText}>Adicionar Produto</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -86,8 +109,8 @@ export default Produtos;
 const styles = StyleSheet.create({
   produtos: {
     flex: 1,
-    alignItems: 'center',
-    width: '100%',
+    alignItems: "center",
+    width: "100%",
   },
   lista: {
     flex: 1,
@@ -98,7 +121,7 @@ const styles = StyleSheet.create({
     padding: 5,
     borderRadius: 7,
     gap: 0,
-    width: '50%',
+    width: "50%",
   },
   header: {
     marginTop: 40,
@@ -113,17 +136,16 @@ const styles = StyleSheet.create({
     width: "80%",
     backgroundColor: "blue",
     justifyContent: "center",
-    alignItems: 'center',
+    alignItems: "center",
     borderRadius: 50,
-
   },
 
   ver: {
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     borderRadius: 8,
     padding: 16,
     marginBottom: 16,
-    shadowColor: '#000',
+    shadowColor: "#000",
     shadowOffset: {
       width: 0,
       height: 2,
@@ -134,40 +156,37 @@ const styles = StyleSheet.create({
   },
   productName: {
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 8,
   },
   button: {
-    backgroundColor: '#0A8DD8',
+    backgroundColor: "#0A8DD8",
     paddingVertical: 6,
     paddingHorizontal: 12,
     borderRadius: 4,
-    alignSelf: 'flex-start',
+    alignSelf: "flex-start",
   },
   buttonText: {
-    color: '#fff',
-    fontWeight: 'bold',
+    color: "#fff",
+    fontWeight: "bold",
     paddingTop: 6,
   },
 
-
   adicionar: {
-    backgroundColor: 'blue',
+    backgroundColor: "blue",
     borderWidth: 2,
-    borderColor: 'blue',
+    borderColor: "blue",
     // paddingVertical: 10,
     // paddingHorizontal: 20,
     borderRadius: 5,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 
   adicionarText: {
-    color: 'white',
+    color: "white",
     fontSize: 17,
-    textAlign: 'center',
-    fontWeight: 'bold',
+    textAlign: "center",
+    fontWeight: "bold",
   },
-
-
 });
